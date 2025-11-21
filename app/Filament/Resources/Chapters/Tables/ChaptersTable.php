89f6695f98ef330db5b7dce7b0afdc13eca81771
crisisstealth <tblanchet7@yahoo.fr>
2025-11-21 14:46:16 +0100

    Affichage des chapitre dans chapters

--- app/Filament/Resources/Chapters/Tables/ChaptersTable.php
+++ app/Filament/Resources/Chapters/Tables/ChaptersTable.php
@@ -7,6 +7,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -16,7 +17,42 @@ class ChaptersTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('numero')
+                    ->label('Item')
+                    ->searchable()
+                    ->sortable()
+                    ->weight('bold'),
+                
+                TextColumn::make('description')
+                    ->label('Description')
+                    ->searchable()
+                    ->limit(100)
+                    ->wrap()
+                    ->grow(),
+                
+                TextColumn::make('learningObjectives_count')
+                    ->label('Objectifs')
+                    ->counts('learningObjectives')
+                    ->sortable()
+                    ->alignCenter(),
+                
+                TextColumn::make('questions_count')
+                    ->label('Questions')
+                    ->counts('questions')
+                    ->sortable()
+                    ->alignCenter(),
+                
+                TextColumn::make('created_at')
+                    ->label('Créé le')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                
+                TextColumn::make('updated_at')
+                    ->label('Modifié le')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 TrashedFilter::make(),
