2ff27b86dbb5da3a440eb2acaae27afe4459de6e
Ragz <ragyedward2001@gmail.com>
2025-11-26 14:31:56 +0100

    DossierForm question form progress

--- app/Filament/Resources/Dossiers/Schemas/DossierForm.php
+++ app/Filament/Resources/Dossiers/Schemas/DossierForm.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Dossiers\Schemas;
 use App\Filament\Resources\Questions\QuestionResource;
 use App\Filament\Resources\Questions\Schemas\QuestionForm;
 use Barryvdh\Debugbar\Facades\Debugbar;
+use Closure;
 use Filament\Forms\Components\Repeater;
 use Filament\Schemas\Components\Form;
 use Filament\Schemas\Components\Utilities\Get;
@@ -16,16 +17,21 @@ class DossierForm
     {
         return $schema
             ->components([
-                Repeater::make("Questions")
+                Repeater::make("questions")
+                ->relationship()
                 ->schema(QuestionForm::schemaArray())
+                ->orderColumn("dossier_order")
+                ->reorderable()
+                ->reorderableWithButtons()
                 ->columnSpanFull()
                 ->collapsible()
+                ->defaultItems(5)
+                ->minItems(3)
+                ->maxItems(8)
                 ->itemLabel(function (Repeater $component, array $state) :string {
                     $numeroQuestionActuelle = array_search(array_search($state, $component->getState()), array_keys($component->getState()))+1;
                    return "Question ".strval($numeroQuestionActuelle);
-                })
-                    // "Question ".strval(array_search($state, $get('')))
-                
+                })                
                 ,
             ]);
     }
