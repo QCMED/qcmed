25f7973f25fa16e6df461fbcd9a8338e2cc84f56
Ragz <ragyedward2001@gmail.com>
2025-11-27 16:42:02 +0100

    add dossier author = auth user at creation

--- app/Filament/Resources/Dossiers/Pages/CreateDossier.php
+++ app/Filament/Resources/Dossiers/Pages/CreateDossier.php
@@ -4,8 +4,15 @@ namespace App\Filament\Resources\Dossiers\Pages;
 
 use App\Filament\Resources\Dossiers\DossierResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateDossier extends CreateRecord
 {
     protected static string $resource = DossierResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = Auth::id();
+        return $data;
+    }
 }
