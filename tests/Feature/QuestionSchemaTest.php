4ca0a2aa7b971814df81da53880e2d8a4f81b043
Ragz <ragyedward2001@gmail.com>
2025-12-02 22:05:32 +0100

    add form error 1st test

--- tests/Feature/QuestionSchemaTest.php
+++ tests/Feature/QuestionSchemaTest.php
@@ -3,28 +3,43 @@
 // use App\Filament\Resources\Questions\Pages\CreateQuestion;
 
 use App\Filament\Resources\Questions\Pages\CreateQuestion;
+use App\Filament\Resources\Questions\Pages\EditQuestion;
 use App\Filament\Resources\Questions\Pages\ListQuestions;
 use App\Models\User;
+use Database\Seeders\ChaptersSeeder;
+use Database\Seeders\ChaptersSeederComplete;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Livewire\livewire;
 
-uses(Illuminate\Foundation\Testing\DatabaseMigrations::class);
+uses(Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 
 beforeEach(function () {
     $user = User::factory()->create(['role' => User::ROLE_ADMIN]);
-    
-
     actingAs($user);
-    
 });
 
 
-test('can load the page', function () {
-    $users = User::factory()->count(5)->create();
+test('can load the question create form', function () {
 
     livewire(CreateQuestion::class)
         ->assertOk();
-        // ->assertCanNotSeeTableRecords($users);
+});
+
+// test('can load the question edit form', function () {
+
+//     livewire(EditQuestion::class)
+//         ->assertOk();
+// });
+
+test('form has errors', function () {
+    $this->seed(ChaptersSeeder::class);
+    livewire(CreateQuestion::class)
+        ->fillForm([
+            // 'title' => fake()->sentence(),
+            'chapter' => "50",
+        ])
+        ->call("create")
+        ->assertHasFormErrors();
 });
