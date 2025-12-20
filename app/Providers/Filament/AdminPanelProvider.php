b57304aac5445d218fe901481943abcb3738e8b6
Ragz <ragyedward2001@gmail.com>
2025-12-20 14:25:40 +0100

    change color dash to blue (pour des raisons évidentes *ahem*)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->brandName('QCM Med - Admin')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => "#0e145b",
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
