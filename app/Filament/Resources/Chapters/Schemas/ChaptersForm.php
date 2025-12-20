f00081a7a3ccd0e020ff8a2b5f3668165af9d3ee
Ragz <ragyedward2001@gmail.com>
2025-12-20 15:39:19 +0100

    Formulaire chapitre # Description
    Pull request pour créer un formulaire pour la création de chapitres depuis le dash admin
    
    Pas de dépendance supplémenataire requis
    
    # Type de changement
    
    Supprimer les options qui ne sont pas pertinents pour ce pull request
    
    - [ ] Nouvelle fonctionnalité (sans casser d'autres fonctionnalités)
    
    # Comment ces changements ont été testé?
    
    Description des nouveaux tests, avec les éventuelles instruction à suivre pour les reproduire
    
    - [ ] Test A
    - [ ] Test B
    
    # Checklist:
    
    - [ ] Mon code suit les guidelines de style de ce projet
    - [ ] J'ai fait une première révision de mon code
    - [ ] J'ai commenté mon code dans les parties difficiles à comprendre
    - [ ] J'ai changé la documentation comme j'en ai besoin
    - [ ] Mes changements ne soulèvent pas de nouveaux warnings
    - [ ] J'ai ajouté des tests qui vérifient que ma fonctionnalité ou mes fix marchent comme il le faut
    - [ ] Tous les tests nouveaux et déjà existants sont valides
    - [ ] Tous les changements dont dépend ce PR ont été merge avant de le soumettre à révision

--- app/Filament/Resources/Chapters/Schemas/ChaptersForm.php
+++ app/Filament/Resources/Chapters/Schemas/ChaptersForm.php
@@ -2,6 +2,12 @@
 
 namespace App\Filament\Resources\Chapters\Schemas;
 
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Schemas\Schema;
 
 class ChaptersForm
@@ -10,7 +16,52 @@ class ChaptersForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make("numero")
+                    ->label("Numéro")
+                    ->numeric()
+                    ->unique()
+                    ->required(),
+
+                RichEditor::make("description")
+                    ->required()
+                    ->columnSpanFull(),
+
+                Repeater::make("learningObjectives")
+                    ->required()
+                    ->columnSpanFull()
+                    ->relationship()
+                    ->columns(3)
+                    ->components([
+                        ToggleButtons::make("rang")
+                        ->label("Rang de la connaisance")
+                        ->required()
+                        ->grouped()
+                        ->options([
+                            "A"=>"A",
+                            "B"=>"B"
+                        ]),
+                        Select::make("rubrique")
+                        ->label("Rubrique")
+                        ->options([
+                            "Définition",
+                            "Prise En Charge",
+                            "Evaluation",
+                            "Epidémiologie",
+                            "Physiopathologie",
+                            "Diagnostic Positif",
+                            "Suivi et/ou pronostic",
+                            "Identifier une urgence",
+                            "Contenu multimédia",
+                            "Étiologies",
+                            "Examens complémentaires"
+                        ])
+                        ->columnSpan(2),
+
+                        TextInput::make("intitule")
+                        ->required()
+                        ->columnSpanFull()
+                    ])
+                    
             ]);
     }
 }
